69d3c404bffbd5a92537ece46c3dbc90c2f3e0b7
Vova Lando <vova.lando@gmail.com>
2024-08-27 12:42:40 +0300

    config+fixed create and get tests

--- tests/order.test.ts
+++ tests/order.test.ts
@@ -4,10 +4,16 @@ import { ApiPromise } from '@polkadot/api';
 
 describe('Order Endpoint Blackbox Tests', () => {
   let api: ApiPromise;
-  const baseUrl = 'http://127.0.0.1:16726'; // Assuming API is running here
+  const baseUrl = process.env.DAEMON_HOST;
+  const ahRpcUrl = process.env.AH_RPC_URL;
+  const dotRpcUrl = process.env.DOT_RPC_URL;
+  const recipientAddressSS = process.env.RECIPIENT_SS;
+  if (!baseUrl || !ahRpcUrl || !dotRpcUrl || !recipientAddressSS) {
+    throw new Error('check all environment variables are defined');
+  }
   const dotOrderData = {
     amount: 100,
-    currency: 'DOT',
+    currency: 'DOTL',
     callback: 'https://example.com/callback'
   };
   const usdcOrderData = {
@@ -15,6 +21,29 @@ describe('Order Endpoint Blackbox Tests', () => {
     currency: 'USDC',
     callback: 'https://example.com/callback'
   };
+
+  const checkOrder = (orderId: any, orderResponseObject:any, orderData: any) => {
+    expect(orderResponseObject).toHaveProperty('order', orderId);
+    expect(orderResponseObject).toHaveProperty('message', '');
+    expect(orderResponseObject).toHaveProperty('recipient');
+    expect(orderResponseObject).toHaveProperty('server_info');
+    expect(orderResponseObject).toHaveProperty('withdrawal_status', 'waiting');
+    expect(orderResponseObject).toHaveProperty('payment_status', 'pending');
+    expect(orderResponseObject).toHaveProperty('amount', orderData.amount);
+
+    expect(orderResponseObject).toHaveProperty('callback', orderData.callback);
+    expect(orderResponseObject).toHaveProperty('transactions');
+    expect(Array.isArray(orderResponseObject.transactions)).toBe(true);
+    expect(orderResponseObject).toHaveProperty('payment_account');
+    expect(orderResponseObject).toHaveProperty('death');
+    expect(orderResponseObject).toHaveProperty('payment_page', '');
+    expect(orderResponseObject).toHaveProperty('redirect_url', '');
+
+    expect(orderResponseObject.server_info).toHaveProperty('version', '0.2.0-rc3');
+    expect(orderResponseObject.server_info).toHaveProperty('instance_id');
+    expect(orderResponseObject.server_info).toHaveProperty('debug', true);
+  }
+
   const generateRandomOrderId = () => {
     return `order_${Math.random().toString(36).substring(2, 15)}`;
   }
@@ -43,38 +72,64 @@ describe('Order Endpoint Blackbox Tests', () => {
     await api.disconnect();
   });
 
-  it('should create a new order', async () => {
+  it('should create a new DOT order', async () => {
     const orderId = generateRandomOrderId();
     const createdOrder = await createOrder(orderId, dotOrderData);
-    expect(createdOrder).toHaveProperty('order', orderId);
-    expect(createdOrder).toHaveProperty('amount', dotOrderData.amount);
-    expect(createdOrder).toHaveProperty('currency.currency', dotOrderData.currency);
-    expect(createdOrder).toHaveProperty('callback', dotOrderData.callback);
-    expect(createdOrder).toHaveProperty('payment_account');
-    expect(createdOrder.payment_status).toBe('pending');
-    expect(createdOrder.withdrawal_status).toBe('waiting');
-    expect(createdOrder).toHaveProperty('server_info');
-    expect(createdOrder.server_info).toHaveProperty('version');
-    expect(createdOrder.server_info).toHaveProperty('instance_id');
+    checkOrder(orderId, createdOrder, dotOrderData);
+
+    expect(createdOrder).toHaveProperty('currency');
+    expect(createdOrder.currency).toHaveProperty('currency', dotOrderData.currency);
+    expect(createdOrder.currency).toHaveProperty('chain_name', 'rococo');
+    expect(createdOrder.currency).toHaveProperty('kind', 'native');
+    expect(createdOrder.currency).toHaveProperty('decimals', 10);
+    expect(createdOrder.currency).toHaveProperty('rpc_url', dotRpcUrl);
   });
 
-  it('should get order details', async () => {
+  it('should create a new USDC order', async () => {
+    const orderId = generateRandomOrderId();
+    const createdOrder = await createOrder(orderId, usdcOrderData);
+    checkOrder(orderId, createdOrder, usdcOrderData);
+
+    expect(createdOrder).toHaveProperty('currency');
+    expect(createdOrder.currency).toHaveProperty('currency', usdcOrderData.currency);
+    expect(createdOrder.currency).toHaveProperty('chain_name', 'statemint');
+    expect(createdOrder.currency).toHaveProperty('kind', 'asset');
+    expect(createdOrder.currency).toHaveProperty('decimals', 6);
+    expect(createdOrder.currency).toHaveProperty('rpc_url', ahRpcUrl);
+    expect(createdOrder.currency).toHaveProperty('asset_id', 1337);
+  });
+
+  it('should get DOT order details', async () => {
     const orderId = generateRandomOrderId();
     await createOrder(orderId, dotOrderData);
     const orderDetails = await getOrderDetails(orderId);
-    expect(orderDetails).toHaveProperty('order', orderId);
-    expect(orderDetails).toHaveProperty('amount', dotOrderData.amount);
-    expect(orderDetails).toHaveProperty('currency.currency', dotOrderData.currency);
-    expect(orderDetails).toHaveProperty('callback', dotOrderData.callback);
-    expect(orderDetails).toHaveProperty('payment_account');
-    expect(orderDetails.payment_status).toBe('pending');
-    expect(orderDetails.withdrawal_status).toBe('waiting');
-    expect(orderDetails).toHaveProperty('server_info');
-    expect(orderDetails.server_info).toHaveProperty('version');
-    expect(orderDetails.server_info).toHaveProperty('instance_id');
+
+    checkOrder(orderId, orderDetails, dotOrderData);
+
+    expect(orderDetails).toHaveProperty('currency');
+    expect(orderDetails.currency).toHaveProperty('currency', dotOrderData.currency);
+    expect(orderDetails.currency).toHaveProperty('chain_name', 'rococo');
+    expect(orderDetails.currency).toHaveProperty('kind', 'native');
+    expect(orderDetails.currency).toHaveProperty('decimals', 10);
+    expect(orderDetails.currency).toHaveProperty('rpc_url', dotRpcUrl);
   });
 
-  it('should return 404 for non-existing order on get order', async () => {
+  it('should get USDC order details', async () => {
+    const orderId = generateRandomOrderId();
+    await createOrder(orderId, usdcOrderData);
+    const orderDetails = await getOrderDetails(orderId);
+    checkOrder(orderId, orderDetails, usdcOrderData);
+
+    expect(orderDetails).toHaveProperty('currency');
+    expect(orderDetails.currency).toHaveProperty('currency', usdcOrderData.currency);
+    expect(orderDetails.currency).toHaveProperty('chain_name', 'statemint');
+    expect(orderDetails.currency).toHaveProperty('kind', 'asset');
+    expect(orderDetails.currency).toHaveProperty('decimals', 6);
+    expect(orderDetails.currency).toHaveProperty('rpc_url', ahRpcUrl);
+    expect(orderDetails.currency).toHaveProperty('asset_id', 1337);
+  });
+
+  xit('should return 404 for non-existing order on get order', async () => {
     const nonExistingOrderId = 'nonExistingOrder123';
     const response = await request(baseUrl)
       .post(`/v2/order/${nonExistingOrderId}`);
--- tests/status.test.ts
+++ tests/status.test.ts
@@ -1,7 +1,10 @@
 import request from 'supertest';
 
 describe('Blackbox API Tests', () => {
-  const baseUrl = 'http://127.0.0.1:16726';
+  const baseUrl = process.env.DAEMON_HOST;
+  if (!baseUrl) {
+    throw new Error('DAEMON_HOST environment variable is not defined');
+  }
 
   it('should return server status with valid server_info properties', async () => {
     const response = await request(baseUrl).get('/v2/status');
@@ -18,6 +21,28 @@ describe('Blackbox API Tests', () => {
     expect(response.body.server_info.debug).toBe(true);
 
     expect(response.body).toHaveProperty('supported_currencies');
-    expect(response.body.supported_currencies).toEqual({}); // Expecting an empty object as per the response
+
+    expect(response.body.supported_currencies).toEqual({
+      "DOTL": {
+        "chain_name": "rococo",
+        "decimals": 10,
+        "kind": "native",
+        "rpc_url": "wss://node-polkadot.zymologia.fi",
+      },
+      "USDC": {
+        "asset_id": 1337,
+        "chain_name": "statemint",
+        "decimals": 6,
+        "kind": "asset",
+        "rpc_url": "wss://node-polkadot-ah.zymologia.fi",
+      },
+      "USDt": {
+        "asset_id": 1984,
+        "chain_name": "statemint",
+        "decimals": 6,
+        "kind": "asset",
+        "rpc_url": "wss://node-polkadot-ah.zymologia.fi",
+      },
+    });
   });
 });
