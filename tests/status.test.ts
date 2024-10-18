7a1799c35d50fa43b00c2fd8f3495dbb02957ac4
Vova Lando <vova.lando@gmail.com>
2024-10-19 01:48:54 +0300

    fixed health tests

--- tests/status.test.ts
+++ tests/status.test.ts
@@ -6,43 +6,63 @@ describe('Blackbox API Tests', () => {
     throw new Error('DAEMON_HOST environment variable is not defined');
   }
 
-  it('should return server status with valid server_info properties', async () => {
+  it('should return server status with valid server_info and supported_currencies', async () => {
     const response = await request(baseUrl).get('/v2/status');
 
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('description');
-    expect(response.body.description).toBe('tododododododo');
 
+    // Validate server_info properties
     expect(response.body).toHaveProperty('server_info');
     expect(response.body.server_info).toHaveProperty('version');
-    expect(response.body.server_info.version).toBe('0.2.0-rc3');
     expect(response.body.server_info).toHaveProperty('instance_id');
     expect(response.body.server_info).toHaveProperty('debug');
     expect(response.body.server_info.debug).toBe(true);
+    expect(response.body.server_info).toHaveProperty('kalatori_remark');
 
+    // Validate supported_currencies properties
     expect(response.body).toHaveProperty('supported_currencies');
 
-    expect(response.body.supported_currencies).toEqual({
-      "DOTL": {
-        "chain_name": "rococo",
-        "decimals": 10,
-        "kind": "native",
-        "rpc_url": "wss://node-polkadot.zymologia.fi",
+    const expectedCurrencies = {
+      DOT: {
+        chain_name: 'polkadot',
+        decimals: 10,
+        kind: 'native',
+        rpc_url: 'wss://1rpc.io/dot',
+        ss58: 0,
       },
-      "USDC": {
-        "asset_id": 1337,
-        "chain_name": "statemint",
-        "decimals": 6,
-        "kind": "asset",
-        "rpc_url": "wss://node-polkadot-ah.zymologia.fi",
+      USDC: {
+        asset_id: 1337,
+        chain_name: 'statemint',
+        decimals: 6,
+        kind: 'asset',
+        rpc_url: 'wss://statemint-rpc.dwellir.com',
+        ss58: 0,
       },
-      "USDt": {
-        "asset_id": 1984,
-        "chain_name": "statemint",
-        "decimals": 6,
-        "kind": "asset",
-        "rpc_url": "wss://node-polkadot-ah.zymologia.fi",
+      USDt: {
+        asset_id: 1984,
+        chain_name: 'statemint',
+        decimals: 6,
+        kind: 'asset',
+        rpc_url: 'wss://statemint-rpc.dwellir.com',
+        ss58: 0,
       },
-    });
+    };
+
+    for (const currency in expectedCurrencies) {
+      if (expectedCurrencies.hasOwnProperty(currency)) {
+        const currencyKey = currency as keyof typeof expectedCurrencies;
+        expect(response.body.supported_currencies).toHaveProperty(currency);
+        expect(response.body.supported_currencies[currency]).toHaveProperty('chain_name', expectedCurrencies[currencyKey].chain_name);
+        expect(response.body.supported_currencies[currency]).toHaveProperty('decimals', expectedCurrencies[currencyKey].decimals);
+        expect(response.body.supported_currencies[currency]).toHaveProperty('rpc_url');
+
+        // Check if asset_id exists in the expected currency and then validate it
+        if ('asset_id' in expectedCurrencies[currencyKey]) {
+          expect(response.body.supported_currencies[currency]).toHaveProperty('asset_id', expectedCurrencies[currencyKey].asset_id);
+        }
+
+        expect(response.body.supported_currencies[currency]).toHaveProperty('ss58', expectedCurrencies[currencyKey].ss58);
+      }
+    }
   });
 });
